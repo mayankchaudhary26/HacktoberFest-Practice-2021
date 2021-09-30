50bdcab0c62d32da99f19964040ce05758f07d2d
Naman Tomar <56973100+Naman5tomar@users.noreply.github.com>
2021-10-01 00:26:20 +0530

    Update Dice Roll Simulator.py

--- Dice Roll Simulator.py
+++ Dice Roll Simulator.py
@@ -17,3 +17,7 @@ while roll_again == "yes" or roll_again == "y":
     print(random.randint(min_val, max_val))
     
     #generating and printing 2nd random integer from 1 to 6
+    print(random.randint(min_val, max_val))
+    
+    #asking user to roll the dice again. Any input other than yes or y will terminate the loop
+    roll_again = input("Roll the Dices Again?") 
