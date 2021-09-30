a8617eca8c946f984bc172af8511499eca261876
ashwin_saxena <86488061+ashwin1612@users.noreply.github.com>
2021-10-01 00:31:54 +0530

    Update Rock-paper-scissors.py

--- Rock-paper-scissors.py
+++ Rock-paper-scissors.py
@@ -5,3 +5,13 @@ player = False
 cpu_score = 0
 player_score = 0
 while True:
+  
+      player = input("Rock, Paper or  Scissors?").capitalize()
+    ## Conditions of Rock,Paper and Scissors
+    if player == computer:
+        print("Tie!")
+    elif player == "Rock":
+        if computer == "Paper":
+            print("You lose!", computer, "covers", player)
+            cpu_score+=1
+        else:
