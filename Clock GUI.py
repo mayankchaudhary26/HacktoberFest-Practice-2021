35abd641d35553ce2e4001adc4dcfdf6a3876162
Parth <91593847+Duisguised1dude@users.noreply.github.com>
2021-10-01 00:58:48 +0530

    Update Clock GUI.py

--- Clock GUI.py
+++ Clock GUI.py
@@ -4,3 +4,8 @@ app_window = Tk()
 app_window.title("Digital Clock") 
 app_window.geometry("420x150") 
 app_window.resizable(1,1)
+
+text_font= ("Boulder", 68, 'bold')
+background = "#f2e750"
+foreground= "#363529"
+border_width = 25
