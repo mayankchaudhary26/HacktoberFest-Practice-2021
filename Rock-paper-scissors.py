96dcaac0d5c8eb485cb647e29d9a005dd97ac8a6
ashwin_saxena <86488061+ashwin1612@users.noreply.github.com>
2021-10-01 00:32:59 +0530

    Update Rock-paper-scissors.py

--- Rock-paper-scissors.py
+++ Rock-paper-scissors.py
@@ -15,3 +15,10 @@ while True:
             print("You lose!", computer, "covers", player)
             cpu_score+=1
         else:
+                     print("You win!", player, "smashes", computer)
+            player_score+=1
+    elif player == "Paper":
+        if computer == "Scissors":
+            print("You lose!", computer, "cut", player)
+            cpu_score+=1
+        else:
