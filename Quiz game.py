3cb8a5085e19a920a94e850acbdaa56b24258266
Prashant Rana <91586269+Prashant111tt@users.noreply.github.com>
2021-10-01 00:42:01 +0530

    Update Quiz game.py

--- Quiz game.py
+++ Quiz game.py
@@ -4,3 +4,11 @@ def check_guess(guess, answer):
     attempt = 0
     while still_guessing and attempt < 3:
         if guess.lower() == answer.lower():
+           print("Correct Answer")
+            score = score + 1
+            still_guessing = False
+        else:
+            if attempt < 2:
+                guess = input("Sorry Wrong Answer, try again")
+            attempt = attempt + 1
+    if attempt == 3:
