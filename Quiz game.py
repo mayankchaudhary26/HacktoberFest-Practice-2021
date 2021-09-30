568003c7930f9d799d953495be33d74098d751f7
Prashant Rana <91586269+Prashant111tt@users.noreply.github.com>
2021-10-01 00:44:22 +0530

    Update Quiz game.py

--- Quiz game.py
+++ Quiz game.py
@@ -12,9 +12,11 @@ def check_guess(guess, answer):
                 guess = input("Sorry Wrong Answer, try again")
             attempt = attempt + 1
     if attempt == 3:
-        print("The Correct answer is ",answer )
+       print("The Correct answer is ",answer )
     
 score = 0
 print("Guess the Animal")
 guess1 = input("Which bear lives at the North Pole? ")
 check_guess(guess1, "polar bear")
+guess2 = input("Which is the fastest land animal? ")
+check_guess(guess2, "Cheetah")
