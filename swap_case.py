afe8a88e3f706c9e773ad6cbe31e24b05b2e2327
swetaKR17 <1804553@kiit.ac.in>
2021-10-02 00:39:56 +0530

    Program_to_Swap_Cases

--- swap_case.py
+++ swap_case.py
@@ -7,3 +7,11 @@ def swap_case(s):
             res=res+letter.lower()
     return res
     
+        
+        
+    
+
+if __name__ == '__main__':
+    s = input()
+    result = swap_case(s)
+    print(result)
