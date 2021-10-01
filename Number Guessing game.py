eca241787b2069ee1c45f09709ab1e970399510c
yoursDaddy007 <35391308+yoursDaddy007@users.noreply.github.com>
2021-10-01 13:02:10 +0530

    Update Number Guessing game.py

--- Number Guessing game.py
+++ Number Guessing game.py
@@ -6,3 +6,8 @@ n = random.randrange(1,100)
 guess = int(input("Enter any number: "))
 while n!= guess: # means if n is not equal to the input guess
     # if guess is smaller than n
+    if guess < n:
+        print("Too low")
+        # to again ask for input
+        guess = int(input("Enter number again: "))
+    # if guess is greater than n
