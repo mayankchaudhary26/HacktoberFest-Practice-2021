f0a25066564262ca10351fafc048d469a887fd13
Anil Kumar <70566960+Dr-Anil@users.noreply.github.com>
2021-10-01 08:40:06 +0530

    Update Alarm clock.py

--- Alarm clock.py
+++ Alarm clock.py
@@ -3,3 +3,9 @@ from playsound import playsound
 alarm_time = input("Enter the time of alarm to be set:HH:MM:SS\n")
 alarm_hour=alarm_time[0:2]
 alarm_minute=alarm_time[3:5]
+alarm_seconds=alarm_time[6:8]
+alarm_period = alarm_time[9:11].upper()
+print("Setting up alarm..")
+while True:
+    now = datetime.now()
+    current_hour = now.strftime("%I")
