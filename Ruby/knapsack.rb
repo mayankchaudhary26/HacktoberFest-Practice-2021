92f7153bcd6d797e9c3ca0d3c8e3961526ac0570
Itasha Modi <58629614+Itashamodi@users.noreply.github.com>
2021-10-04 12:00:03 +0530

    Create knapsack.rb

---
+++ Ruby/knapsack.rb
@@ -0,0 +1,11 @@
+class Knapsack
+
+  attr_accessor :chromosome, :total_weight, :total_value
+
+  def initialize(chromosome)
+    @chromosome = chromosome
+    total_weight = 0.0
+    total_value = 0.0
+  end
+
+end
