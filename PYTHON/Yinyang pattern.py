6a3e8aa1aedbee051c30a8445c2570138f50a28d
Charonyx <churairat.onn@gmail.com>
2021-10-05 17:10:13 +0700

    Add yinyang again

--- PYTHON/Yinyang pattern.py
+++ PYTHON/Yinyang pattern.py
@@ -1,19 +1,20 @@
-# Author      : Charonyx, KMITL
+# Author      : Charonyx, KMITL @ Thailand
 
 # Title       : YIN YANG PATTERN
 
-num = int(input('Enter Input : '))
-k   = num + 2
-if num > 0 :
-    for i in range(-k, k + 1) :
-        for j in range(-k, k + 1) :
-            if not(i == 0 or j == 0) :
-                if abs(i + j) >= k + 2 : print('.', end='')
-                elif j < 0 and ((i < 0 and i + j <= 0) or abs(j) == k or abs(j) == 1 or abs(i) == 1 or abs(i) == 1 or abs(i) == k)  : print('#', end='')
-                elif j > 0 and ((i > 0 and i + j >= 0) or abs(j) == k or abs(j) == 1 or abs(i) == 1 or abs(i) == 1 or abs(i) == k)  : print('+', end='')
-                elif j < 0 : print('+', end='')
-                else : print('#', end='')
-        if not(i == 0 or j == 0) : print('')
+if __name__ == '__main__' :
+    num = int(input('Enter Input : '))
+    k   = num + 2
+    if num > 0 :
+        for i in range(-k, k + 1) :
+            for j in range(-k, k + 1) :
+                if not(i == 0 or j == 0) :
+                    if abs(i + j) >= k + 2 : print('.', end='')
+                    elif j < 0 and ((i < 0 and i + j <= 0) or abs(j) == k or abs(j) == 1 or abs(i) == 1 or abs(i) == 1 or abs(i) == k)  : print('#', end='')
+                    elif j > 0 and ((i > 0 and i + j >= 0) or abs(j) == k or abs(j) == 1 or abs(i) == 1 or abs(i) == 1 or abs(i) == k)  : print('+', end='')
+                    elif j < 0 : print('+', end='')
+                    else : print('#', end='')
+            if not(i == 0 or j == 0) : print('')
 
 '''
 # ----------------------- EXAMPLE ZONE -----------------------
