b65984f5517955cd414fe3a45cb14af9381c9cb9
mohdanwaroddin <92102541+mohdanwaroddin@users.noreply.github.com>
2021-10-09 13:18:30 +0530

    Create  Water Reminder.py
    I developed this program by completing my internship!
    This is so small, but it pays an important role in human life,as often we forget to drink water during our working hours....so worth it.

---
+++ PYTHON/Water Reminder.py
@@ -0,0 +1,10 @@
+import time
+from plyer import notification
+notification.notify(
+    title ='Please drink water',
+    message='water is our life so we should always drink clean water and be healthy!!!',
+    timeout=15
+                 )
+
+  
+  
